5af1f851a8b2b20d5c954e742b5657af7e99b96b
Danses9 <danna.ses9@gmail.com>
2021-07-07 18:05:37 -0300

    Conversão de tipos: Concatenação de números, conversão implícita e ponto flutuante.

---
+++ 4-conversaoDetipos.js
@@ -0,0 +1,14 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+
+console.log(parseInt("2") + parseInt("2"));
+//concatenação dos números.
+
+console.log("10" / "2");
+//conversão implícita.
+
+console.log("Ricardo" / "2"); //not a number.
+console.log(6.5); //ponto flutuante.
+console.log(6,5);
